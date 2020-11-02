3eff9b187603dffc6f8ac00ca8931294adc60c4e
Tariq Ismail <tariqismail@gmail.com>
2020-11-02 13:55:46 +0400

    added edit form and refactored task form as partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,7 +18,18 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
